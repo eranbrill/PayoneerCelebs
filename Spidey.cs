5b597f1477118a742577b56c7fa350925c7104c4
Eran Brill <briler1@gmail.com>
2020-11-05 13:33:00 +0200

    Create Spidey.cs

---
+++ Spidey.cs
@@ -0,0 +1,21 @@
+//Never was so much owed by so many to so few
+
+public Class Spiderman {
+  private Power _power;
+  private Responsiblity _resposiblity; 
+
+  public  Spiderman (Power power, ResposnibilityFactory resposibilityFactory) {
+    this._power= power;
+    if  ("Great" == Power.Level) {
+      this._resposibility= resposibilityFactory.Great();
+  }
+  
+  // Spidy is already coding for us
+  // Click the link bellow if you want to join a great team and do great stuff together (not sure about saving the world)
+  
+}
+ 
+  
+  
+
+  
